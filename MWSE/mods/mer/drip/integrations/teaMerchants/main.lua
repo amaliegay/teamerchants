09906be504584081134e575a7cb927248f641695
JosephMcKean <109032964+JosephMcKean@users.noreply.github.com>
2022-08-04 09:32:42 +0800

    Delete main.lua

--- MWSE/mods/mer/drip/integrations/teaMerchants/main.lua
+++
@@ -1,8 +0,0 @@
-local interop = require("mer.drip")
---Tea Merchants
-local weapons = require("mer.drip.integrations.teaMerchants.weapons")
-for _, weapon in ipairs(weapons) do
-    interop.registerWeapon(weapon)
-end
-
-
