f87ce6dffb43a8cdaa50dc47734dfb0633b49acf
JoanyMcKarelyn <109032964+JoanyMcKarelyn@users.noreply.github.com>
2022-08-27 18:16:12 +0800

    Delete MWSE/mods/teaMerchants directory

--- MWSE/mods/teaMerchants/newBottles.lua
+++
@@ -1,5 +0,0 @@
-local interop = require("mer.ashfall.interop")
-interop.registerWaterContainers({
-	jsmk_Misc_Com_Bottle = { capacity = 90, weight = 3, value = 4, holdsStew = false },
-	includeOverrides = false,
-})
--- MWSE/mods/teaMerchants/validTeas.lua
+++
@@ -1,6 +0,0 @@
-local this = {}
-local teaConfig = require("mer.ashfall.common.common").staticConfigs.teaConfig
-for ingredId, _ in pairs(teaConfig.teaTypes) do
-	table.insert(this, ingredId)
-end
-return this
